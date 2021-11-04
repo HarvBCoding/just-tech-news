96808e7ee1bbfb25a4e05827cfb1832c53d72057
Breania Harvey <harvey.breania@gmail.com>
2021-11-04 03:08:54 -0500

    GET and DELETE routes added for comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
 // prefixes /users to all routes in the user-routes.js file
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 
 module.exports = router;
