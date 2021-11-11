e0dd13c5cd12a9053e484424c9603e0fa8ab6209
Breania Harvey <harvey.breania@gmail.com>
2021-11-11 16:41:59 -0600

    helper functions added

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 // this will setup Handlebars.js as app's template engine of choice
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 // sets up an Express.js session
 const session = require('express-session');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 // connects the session to Sequelize database
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
